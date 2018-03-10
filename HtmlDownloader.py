c63d429312513ca5265198eac8972ddcc6d418c3
王卓君 <wangzhuojun@wangzhuojundeMacBook-Air.local>
2018-03-10 16:12:39 +0800

    Initial commit

---
+++ HtmlDownloader.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+import requests
+
+class HtmlDownloader(object):
+
+    def download(self,url):
+        if url is None:
+            return None
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel …) Gecko/20100101 Firefox/58.0'
+        headers = {'User-Agent':user_agent}
+        r = requests.get(url, headers=headers)
+        if r.status_code==200:
+            r.encoding='utf-8'
+            return r.text
+        return None
+    
